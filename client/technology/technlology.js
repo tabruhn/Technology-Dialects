aed6e2fc33096321a31f976ae999b9413bc6b7aa
TaylorAckley <taylorackley@gmail.com>
2015-10-10 21:06:52 -0700

    latest technology template code

--- client/technology/technlology.js
+++ client/technology/technlology.js
@@ -2,8 +2,12 @@ Template.technology.helpers({
 	getglassdoor: function(){
 		var locationParam = Router.current().params.technology || "seattle";
 		console.log(locationParam);
-		var glassdoorresults= ReactiveMethod.call("glassdoor", location);
+		var glassdoorresults= ReactiveMethod.call("glassdoor", locationParam);
 		console.log(glassdoorresults);
 		return glassdoorresults;
+	},
+	getQueryParam: function() {
+		console.log(Router.current().params.technology);
+		return Router.current().params.technology;
 	}
 });
--- server/methods.js
+++ server/methods.js
@@ -3,10 +3,10 @@ Meteor.methods({
 		this.unblock();
 		var loc = location;
 		try {
-			var url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=45427&t.k=f4o1HyUwsiU&userip=199.59.107.218&useragent=Chrome/45.0.2454.101&action=jobs-stats&returnStates=true&l="+ loc +"&q=javascript";
+			var url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=45427&t.k=f4o1HyUwsiU&userip=199.59.107.218&useragent=Chrome/45.0.2454.101&action=jobs-stats&returnStates=true&l=seattle&q=javascript";
 			var result = Meteor.http.get(url);
-			//console.log(result.data.response);
-			return result.data.response.cities;
+			console.log(result.data.response);
+			return result.data.response;
 		} catch (error){
 			console.log(error);
 			return error;
