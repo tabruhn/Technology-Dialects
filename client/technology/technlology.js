62b8256f079b624488d64f08ebe8292bf6c808a6
Xenoxo <timwang@Tims-MacBook-Air.local>
2015-10-10 20:40:48 -0700

    Added method to get API return

--- client/technology/technlology.js
+++ client/technology/technlology.js
@@ -1,6 +1,8 @@
 Template.technology.helpers({
 	getglassdoor: function(){
-		var glassdoorresults= ReactiveMethod.call("glassdoor");
+		var locationParam = Router.current().params.technology || "seattle";
+		console.log(locationParam);
+		var glassdoorresults= ReactiveMethod.call("glassdoor", location);
 		console.log(glassdoorresults);
 		return glassdoorresults;
 	}
--- server/methods.js
+++ server/methods.js
@@ -1,11 +1,12 @@
 Meteor.methods({
-	glassdoor: function (){
+	glassdoor: function (location){
 		this.unblock();
+		var loc = location;
 		try {
-			var url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=45427&t.k=f4o1HyUwsiU&userip=199.59.107.218&useragent=Chrome/45.0.2454.101&action=jobs-stats&q=javascript&returnCities=true";
+			var url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=45427&t.k=f4o1HyUwsiU&userip=199.59.107.218&useragent=Chrome/45.0.2454.101&action=jobs-stats&returnStates=true&l="+ loc +"&q=javascript";
 			var result = Meteor.http.get(url);
-			console.log(result);
-			return result;
+			//console.log(result.data.response);
+			return result.data.response.cities;
 		} catch (error){
 			console.log(error);
 			return error;
