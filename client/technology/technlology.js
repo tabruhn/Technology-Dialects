be43ca472243d5b08efa90dffdd9d534f614a9b5
TaylorAckley <taylorackley@gmail.com>
2015-10-10 21:54:26 -0700

    glassdoor properly pulls back data based on technology and location params

--- client/technology/technlology.js
+++ client/technology/technlology.js
@@ -1,8 +1,9 @@
 Template.technology.helpers({
 	getglassdoor: function(){
-		var locationParam = Router.current().params.technology || "seattle";
+		var locationParam = Router.current().params.location || "seattle";
+		var technologyParam = Router.current().params.technology || "javascript";
 		console.log(locationParam);
-		var glassdoorresults= ReactiveMethod.call("glassdoor", locationParam);
+		var glassdoorresults= ReactiveMethod.call("glassdoor", locationParam, technologyParam);
 		console.log(glassdoorresults);
 		return glassdoorresults;
 	},
--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,11 @@
 Meteor.methods({
-	glassdoor: function (location){
+	glassdoor: function (technology, location){
+		console.log(location);
+		console.log(technology);
 		this.unblock();
-		var loc = location;
 		try {
-			var url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=45427&t.k=f4o1HyUwsiU&userip=199.59.107.218&useragent=Chrome/45.0.2454.101&action=jobs-stats&returnStates=true&l=seattle&q=javascript";
+			var url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=45427&t.k=f4o1HyUwsiU&userip=199.59.107.218&useragent=Chrome/45.0.2454.101&action=jobs-stats&returnCities=true&l=" + location + "&q=" + technology;
+			console.log(url);
 			var result = Meteor.http.get(url);
 			console.log(result.data.response);
 			return result.data.response;
